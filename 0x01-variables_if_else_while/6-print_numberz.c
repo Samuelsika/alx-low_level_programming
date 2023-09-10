4e564582ad5f315b85f23ad35557c2d7ce067107
Samuelsika <samuelkofisikatetteh@gmail.com>
2023-09-10 15:28:24 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int q;
+
+	q = 0;
+	while
+		(q < 10) {
+			putchar(q + '0');
+			q++;
+		}
+	putchar('\n');
+	return (0);
+}
+		
