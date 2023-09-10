4be7723c260696de0c3ac788ab089876b6acb23d
Samuelsika <samuelkofisikatetteh@gmail.com>
2023-09-10 13:27:17 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char e;
+
+	c = 'a';
+	e = 'A';
+	while
+	    
+	       (c <= 'z') {
+		       putchar(c);
+		       c++;
+	       }
+         while
+
+	       (e <= 'Z') {
+		      putchar (e);
+		      e++;
+	       }
+          putchar('\n');
+	  return (0);
+}	  
