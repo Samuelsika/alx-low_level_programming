b7b5665f5813e081ac77cbbafe8c78d9da57b3d2
Samuelsika <samuelkofisikatetteh@gmail.com>
2023-09-10 15:58:24 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	int h;
+
+	c = 'a';
+	h = 0;
+	while
+		(h < 10) {
+			putchar(h + '0');
+			h++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
